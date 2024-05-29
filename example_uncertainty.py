566c18616ad87a7ba77bb111afdfc09da935dd70
autoblack_push <${GITHUB_ACTOR}@users.noreply.github.com>
2024-05-29 10:36:18 +0000

    fixup! Format Python code with psf/black push

--- example_uncertainty.py
+++ example_uncertainty.py
@@ -11,15 +11,17 @@ iris_df = pd.DataFrame(iris_obj.data, columns=iris_obj.feature_names)
 iris_df["species"] = [iris_obj.target_names[s] for s in iris_obj.target]
 
 # Default uncertainty plot
-uncertainty_plot(
-    data=iris_df, x="sepal width (cm)", y="species", iterations=100
-)
+uncertainty_plot(data=iris_df, x="sepal width (cm)", y="species", iterations=100)
 plt.savefig("./docs/img/uncertainty_default.png", dpi=150)
 plt.show()
 
 # Using jackknife instead of resample to assess uncertainty
 uncertainty_plot(
-    data=iris_df, x="sepal width (cm)", y="species", iterations=100, jackknife_fraction=0.8
+    data=iris_df,
+    x="sepal width (cm)",
+    y="species",
+    iterations=100,
+    jackknife_fraction=0.8,
 )
 plt.savefig("./docs/img/uncertainty_jackknife.png", dpi=150)
 plt.show()
@@ -29,14 +31,24 @@ from matplotlib.colors import ListedColormap
 
 colormap = ListedColormap(["red", "green", "blue"])
 uncertainty_plot(
-    data=iris_df, x="sepal width (cm)", y="species", iterations=100, mode="resample", colormap=colormap,
+    data=iris_df,
+    x="sepal width (cm)",
+    y="species",
+    iterations=100,
+    mode="resample",
+    colormap=colormap,
 )
 plt.savefig("./docs/img/uncertainty_custom_color.png", dpi=150)
 plt.show()
 
 # Uncertainty plot with a confounder
 uncertainty_plot(
-    data=iris_df, x="sepal width (cm)", y="species", iterations=100, mode="resample", confounders=[("petal width (cm)", 1)]
+    data=iris_df,
+    x="sepal width (cm)",
+    y="species",
+    iterations=100,
+    mode="resample",
+    confounders=[("petal width (cm)", 1)],
 )
 plt.savefig("./docs/img/uncertainty_confounder.png", dpi=150)
 plt.show()
